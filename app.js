0bcde16291ee0d2e48adf53ba8b1a34a6e027c5c
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-02-21 08:44:34 +0530

    file2

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ filter.addEventListener('keyup', filterItem);
 function addItem(e){
     e.preventDefault();
 
-var firstName = document.getElementById('form-input1').value;
-var lastName = document.getElementById('form-input2').value;
+var name = document.getElementById('form-input1').value;
+var email = document.getElementById('form-input2').value;
+var contact = document.getElementById('form-input3').value;
 
-firstName = firstName +" "+ lastName;
+var userDetail = name +"-"+ email + "-" + contact;
 
 var li = document.createElement('li');
-li.appendChild(document.createTextNode(firstName));
+li.appendChild(document.createTextNode(userDetail));
 li.className = 'list-group-item';
 
 var del = document.createElement('button');
@@ -38,10 +39,11 @@ itemList.appendChild(li);
 
 // localStorage.setItem('userData',JSON.stringify(firstName));
 
-myObj.push(firstName);
+myObj.push(userDetail);
 localStorage.setItem('userData',JSON.stringify(myObj));
 
 document.getElementById('form-input1').value = '';
+document.getElementById('form-input2').value = '';
 document.getElementById('form-input2').value = '';
 }
 
